6eaab4dd41dc48e5e4950dd5c7a90172ddcbeeda
Phil Wells <phil.wells@epicgames.com>
2026-08-17 13:56:56 -0400

    load checkerboard pattern

--- src/main.asm
+++ src/main.asm
@@ -4,4 +4,45 @@ SECTION "Entry Point", ROM0[$100]
 
 SECTION "Main", ROM0[$150]
 Start:
-	jr Start
+.waitVBlank:
+	ldh a, [$ff44]
+	cp a, 144
+	jr c, .waitVBlank
+
+	ld hl, $ff40
+	res 7, [hl]
+
+	ld de, TileData
+	ld hl, $8000
+	ld c, 16
+
+.copyTile
+	ld a, [de]
+	ld [hl+], a
+	inc de
+	dec c
+	jr nz, .copyTile
+
+	ld hl, $9800
+	ld a, 0
+	ld b,4
+.clearMapOuter
+	ld c, 0
+.clearMapInner
+	ld [hl+], a
+	dec c
+	jr nz, .clearMapInner
+	dec b
+	jr nz, .clearMapOuter
+
+	ld a, $e4
+	ldh [$ff47], a
+
+	ld a, %10010001
+	ldh [$ff40], a
+
+.hang
+	jr .hang
+
+TileData:
+    db $55, $55, $AA, $AA, $55, $55, $AA, $AA, $55, $55, $AA, $AA, $55, $55, $AA, $AA
